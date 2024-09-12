18abdee7420864090cc14a89aabff215ba1fab71
nam-97 <nam.nx@joonsolutions.com>
2024-09-12 08:55:42 +0700

    add int models

---
+++ models/int/int_ticket_flights__joined.sql
@@ -0,0 +1,23 @@
+with ticket_flights as (
+    select * from {{ ref('stg_booking__ticket_flights') }}
+),
+
+tickets as (
+    select * from {{ ref('stg_booking__tickets') }}
+)
+
+select
+    ticket_flights.ticket_flight_id,
+    ticket_flights.flight_id,
+    ticket_flights.fare_conditions,
+    ticket_flights.ticket_no,
+    tickets.book_ref,
+    tickets.passenger_id,
+    tickets.passenger_name,
+    tickets.is_lead_booking_passenger,
+
+    {{ dbt_utils.generate_surrogate_key(['tickets.book_ref','ticket_flights.flight_id']) }} as book_flight_id
+
+from ticket_flights
+inner join tickets
+    on ticket_flights.ticket_no = tickets.ticket_no
